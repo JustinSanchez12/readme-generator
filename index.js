9941eab085d25b499478317ef427dc7c7d83fd87
Justin Sanchez <justinlsanchez12@gmail.com>
2023-05-20 15:00:04 -0700

    added the fs writefile and generated markdown text

--- index.js
+++ index.js
@@ -2,10 +2,67 @@
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 const fs = require('fs');
-const { default: inquirer } = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [
+//Created an array of questions for user input using inquirer node modules
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'title',
+            messsage: 'Title'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            messsage: 'Description'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            messsage: 'Installation'
+        },
+        {
+            type: 'input',
+            name: 'table of contents',
+            message: 'Table of Contents'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Usage'
+        },
+        {
+            type: 'input',
+            name: 'lincense',
+            message: 'License'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Contributing'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Tests'
+        },
+        {
+            type: 'input',
+            name: 'questions',
+            message: 'Questions'
+        }
+    ])
+    //this function will generate the markdown file for the ReadMe.md
+    .then((answers) =>{
+        const markdownText = generateMarkdown(answers);
+        //this will write the file and if no errors will generate a message that it is successul
+        fs.writeFile('ReadMe.md', markdownText, (err) =>
+            err ? console.log(err) : console.log('Successfully created ReadME.md'));
+    });
+
+
+
+/*const questions = [
     {
         type: 'input',
         name: 'title',
@@ -68,4 +125,4 @@ function init() {
 }
 
 // Function call to initialize app
-init();
+init();*/
