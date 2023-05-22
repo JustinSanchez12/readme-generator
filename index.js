317316d78a130c7915a0b9d26717186a29ce6969
Justin Sanchez <justinlsanchez12@gmail.com>
2023-05-21 23:53:00 -0700

    changed some of the arrays around, deleted questions and changed the license back to input

--- index.js
+++ index.js
@@ -23,18 +23,13 @@ inquirer
         },
         {
             type: 'input',
-            name: 'table of contents',
-            message: 'Table of Contents'
-        },
-        {
-            type: 'input',
             name: 'usage',
             message: 'Usage'
         },
         {
             type: 'input',
             name: 'lincense',
-            message: 'License'
+            message: 'License',
         },
         {
             type: 'input',
@@ -48,81 +43,19 @@ inquirer
         },
         {
             type: 'input',
-            name: 'questions',
-            message: 'Questions'
-        }
+            name: 'github',
+            message: 'Enter your GitHub Username:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your Email:',
+        },
     ])
     //this function will generate the markdown file for the ReadMe.md
     .then((answers) =>{
         const markdownText = generateMarkdown(answers);
         //this will write the file and if no errors will generate a message that it is successul
-        fs.writeFile('ReadMe.md', markdownText, (err) =>
-            err ? console.log(err) : console.log('Successfully created ReadME.md'));
+        fs.writeFile('README.md', markdownText, (err) =>
+            err ? console.log(err) : console.log('Successfully created README.md'));
     });
-
-
-
-/*const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        messsage: 'Title'
-    },
-    {
-        type: 'input',
-        name: 'description',
-        messsage: 'Description'
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        messsage: 'Installation'
-    },
-    {
-        type: 'input',
-        name: 'table of contents',
-        message: 'Table of Contents'
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'Usage'
-    },
-    {
-        type: 'input',
-        name: 'lincense',
-        message: 'License'
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'Contributing'
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'Tests'
-    },
-    {
-        type: 'input',
-        name: 'questions',
-        message: 'Questions'
-    }
-];
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    //TODO add fs code here to save file
-}
-
-// TODO: Create a function to initialize app
-function init() {
-    inquirer.prompt(questions).then(function(answers){
-        const markdownText = generateMarkdown(answers);
-        writeToFile('whatevs', markdownText);
-        //TODO: Finish change filename
-    });
-}
-
-// Function call to initialize app
-init();*/
