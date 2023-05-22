1a40e6cdb790f0d3e261ed205008e8fef601fcde
Justin Sanchez <justinlsanchez12@gmail.com>
2023-05-21 22:18:35 -0700

    created the generateMarkdown function to generate the readme file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,26 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${data.description}
+  ## Table of Contents:
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributions](#contributions)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  ### Installation:
+  ${data.installation}
+  ### Usage
+  ${data.usage}
+  ### License
+  ${data.license}
+  ### Contributions
+  ${data.contributions}
+  ### Tests
+  ${data.tests}
+  ### Questions
+  ${data.questions}
 `;
 }
 
